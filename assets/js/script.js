7d65f22eed0e94e4ad55e9b3b359b81810fab51d
LeonardSuli <leonardsuli1991@gmail.com>
2023-11-20 19:44:19 +0100

    add conditions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 
 //chiedere all'utente i km che vuole percorrere
 
-const km = parseInt(prompt('Quanti km vuoi percorrere?'));
+const km = prompt('Quanti km vuoi percorrere?');
 console.log(km);
 
 //chiedere all'utente l'età
@@ -11,12 +11,29 @@ console.log(età);
 
 //il prezzo del biglietto è definito in base ai km (0.21 € al km)
 
-let prezzo_biglietto = km * 0.21;
+const prezzo_biglietto = km * 0.21;
 console.log(prezzo_biglietto);
 
 //va applicato uno sconto del 20% per i minorenni
 
-// if(età < 18){
-//     let prezzo_biglietto_min18 = prezzo_biglietto * 20%
-// }
+if(età < 18){
+
+    const prezzo_biglietto_min18 = prezzo_biglietto - (prezzo_biglietto * 20 / 100);
+    console.log(prezzo_biglietto_min18);
+    document.writeln(prezzo_biglietto_min18);
+
+    //va applicato uno sconto del 40% per gli over 65 (fortunelli)
+}else if(età > 65){
+
+    const prezzo_biglietto_over65 = prezzo_biglietto - (prezzo_biglietto * 40 / 100);
+    console.log(prezzo_biglietto_over65);
+    document.writeln(prezzo_biglietto_over65);
+
+}else{
+
+    //prezzo normale
+    console.log(prezzo_biglietto);
+    document.writeln(prezzo_biglietto);
+}
+
 
